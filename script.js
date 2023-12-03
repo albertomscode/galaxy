7da04db0851a13a50ee13402d5780f459642ea94
Alberto <alberto.moreno.sn@gmail.com>
2023-12-03 16:16:51 +0000

    Proyect ready to send

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Welcome to the Website
 let question = prompt("Welcome to the Space🚀 Are you ready about what you are going to see here? YES/NO");
 let yes = "YES";
 let no = "NO";
@@ -9,20 +10,3 @@ if (question === yes){
 }
 
 alert("P.S! Look at all animations and transitions I made. I am very proud of it, so I hope you enjoy it😜");
-
-// cookie policy
-$(document).on("ready", function () {
-  if (document.cookie.indexOf("accepted_cookies=") < 0) {
-    $(".cookie-overlay").removeClass("d-none").addClass("d-block");
-  }
-
-  $(".accept-cookies").on("click", function () {
-    document.cookie = "accepted_cookies=yes;";
-    $(".cookie-overlay").removeClass("d-block").addClass("d-none");
-  });
-
-  // expand depending on your needs
-  $(".close-cookies").on("click", function () {
-    $(".cookie-overlay").removeClass("d-block").addClass("d-none");
-  });
-});
